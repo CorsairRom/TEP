80eb34e13151da61995813b85976f21c2670ff8b
CorsairRom <ric.romero@duocuc.cl>
2023-07-11 02:51:11 -0400

    error: filter method not compatible with angular 16

--- src/app/tep-app/empleados/pages/lista-empleados/lista-empleados.component.ts
+++ src/app/tep-app/empleados/pages/lista-empleados/lista-empleados.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { EmpleadosServices } from '../../services/empleados.service';
 import { Empleados } from '../../interfaces/empleados.interface';
 import { Router } from '@angular/router';
 import { MatTableDataSource } from '@angular/material/table';
-
+import { MatTable } from '@angular/material/table';
 
 @Component({
   selector: 'app-lista-empleados',
@@ -22,11 +22,11 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class ListaEmpleadosComponent implements OnInit {
 
-
+  @ViewChild(MatTable) table!: MatTable<any>;
 
   public empleados:Empleados[] = [];
   public displayedColumns: string[] = ['id', 'Nombre', 'Rut', 'Email', 'Empresa', 'Acciones'];
-  public dataSource:any
+  public dataSource!:MatTableDataSource<any>;
 
   constructor(
     private empleadosServices: EmpleadosServices,
@@ -36,8 +36,11 @@ export class ListaEmpleadosComponent implements OnInit {
 
   ngOnInit(): void {
     this.empleadosServices.getEmpleados()
-    .subscribe(empleados => this.empleados = empleados);
-    this.dataSource = new MatTableDataSource(this.empleados);
+    .subscribe(empleados => {
+      this.empleados = empleados
+      console.log(empleados);
+      this.dataSource = new MatTableDataSource(empleados);
+    });
   }
 
   navigate(): void {
@@ -48,7 +51,14 @@ export class ListaEmpleadosComponent implements OnInit {
     const filtro = (event.target as HTMLInputElement).value;
     console.log(filtro);
 
+
     this.dataSource.filter = filtro;
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+    this.table.renderRows();
   }
+
 
 }
